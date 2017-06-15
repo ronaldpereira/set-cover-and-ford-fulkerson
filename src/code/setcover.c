542e5474e7b5086bdda866b8f0b6171b8a89a7f1
Ronald Pereira <ronald.drp11@gmail.com>
2017-06-15 10:58:11 -0300

    Set cover ready, with test prints

--- src/code/setcover.c
+++ src/code/setcover.c
@@ -19,6 +19,47 @@ void matrixPrinter(double **m, int dimx, int dimy)
     }
 }
 
+void printResults(double *x, int *y, int numVertex, int dimy)
+{
+    int i;
+
+    printf("\nX = (");
+    for(i = 0; i < numVertex; i++)
+    {
+        printf("%.3lf", x[i]);
+        if(i < numVertex-1)
+            printf(" ");
+        else
+            printf(")");
+    }
+
+    printf("\nY = (");
+
+    for(i = 0; i < dimy; i++)
+    {
+        printf("%d", y[i]);
+        if(i < dimy-1)
+            printf(" ");
+        else
+            printf(")");
+    }
+    printf("\n");
+}
+
+void printFinalWeight(int *y, double **m, int dimy)
+{
+    int i;
+    double sum = 0;
+
+    for(i = 0; i < dimy; i++)
+    {
+        if(y[i] == 1)
+            sum += m[0][i];
+    }
+
+    printf("Cover Cost = %.3lf\n", sum);
+}
+
 void matrixAlocation(double ***m, int dimx, int dimy)
 {
     int i;
@@ -58,12 +99,32 @@ void matrixReader(char *location, double ***m, int *dimx, int *dimy)
     fclose(input);
 }
 
-int getRandomVertex(bool *covered, int uncoveredCounter, int numVertex)
+int uncoveredCounter(bool *covered, int numVertex)
 {
     int i;
+    int uncoveredCounter = 0;
+
+    printf("\nuncovered ");
+    for(i = 0; i < numVertex; i++)
+    {
+        printf("%d ", covered[i]);
+        if(!(covered[i]))
+            uncoveredCounter++;
+    }
+    printf("\n");
+
+    return uncoveredCounter;
+}
+
+int getRandomVertex(bool *covered, int numVertex)
+{
+    int i;
+    int uncovered = uncoveredCounter(covered, numVertex);
+
+    printf("uncovered = %d\n", uncovered);
 
     srand(time(NULL));
-    int random = rand() % uncoveredCounter;
+    int random = rand() % uncovered;
     printf("\nRandom = %d\n", random);
 
     for(i = 0; i < numVertex; i++)
@@ -72,6 +133,7 @@ int getRandomVertex(bool *covered, int uncoveredCounter, int numVertex)
         {
             if(random == 0)
                 return i;
+
             else
                 random--;
         }
@@ -94,6 +156,26 @@ double getMaxValueOfVertex(double *x, double **m, int dimx, int indexVertex, int
     return (m[0][indexCover] - sum);
 }
 
+int getMajorCover(double **m, int dimx, int indexCover1, int indexCover2)
+{
+    int i;
+    int count1 = 0, count2 = 0;
+
+    for(i = 0; i < dimx; i++)
+    {
+        if(m[i][indexCover1] == 1)
+            count1++;
+    }
+
+    for(i = 0; i < dimx; i++)
+    {
+        if(m[i][indexCover2] == 1)
+            count2++;
+    }
+
+    return (count1 > count2 ? indexCover1 : indexCover2);
+}
+
 void maximizeIndex(bool **covered, double **x, int **y, double **m, int dimx, int dimy, int indexVertex)
 {
     int j;
@@ -111,17 +193,27 @@ void maximizeIndex(bool **covered, double **x, int **y, double **m, int dimx, in
                 printf("minVertexWeight = %.3lf && indexCover = %d\n", minVertexWeight, indexCover);
             }
 
-            // TODO else if(getMaxValueOfVertex(*x, m, dimx, indexVertex, j) == minVertexWeight) Pegar o cover com maior abrangencia
+            else if(getMaxValueOfVertex(*x, m, dimx, indexVertex, j) == minVertexWeight)
+            {
+                indexCover = getMajorCover(m, dimx, indexCover, j);
+            }
         }
     }
 
+    (*x)[indexVertex] = minVertexWeight;
+    (*y)[indexCover] = 1;
 
+    for(j = 1; j < dimx; j++)
+    {
+        if(m[j][indexCover] == 1)
+            (*covered)[j-1] = true;
+    }
 }
 
 void setCover(double ***m, int dimx, int dimy)
 {
     bool *covered;
-    int numVertex = dimx-1, uncoveredCounter = numVertex, indexVertex;
+    int numVertex = dimx-1, indexVertex;
     double *x;
     int *y;
 
@@ -129,16 +221,18 @@ void setCover(double ***m, int dimx, int dimy)
     x = (double*) calloc(numVertex,sizeof(double));
     y = (int*) calloc(dimy,sizeof(int));
 
-    while(uncoveredCounter > 0)
+    while(uncoveredCounter(covered, numVertex) > 0)
     {
-        indexVertex = getRandomVertex(covered, uncoveredCounter, numVertex);
+        indexVertex = getRandomVertex(covered, numVertex);
         printf("indexVertex = %d\n", indexVertex);
         covered[indexVertex] = true;
 
         maximizeIndex(&covered, &x, &y, *m, dimx, dimy, indexVertex);
 
-        uncoveredCounter--;
+        printResults(x, y, numVertex, dimy);
     }
+
+    printFinalWeight(y, *m, dimy);
 }
 
 int main(int argc, char *argv[])
