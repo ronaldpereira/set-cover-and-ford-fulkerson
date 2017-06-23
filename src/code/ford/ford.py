944e5f56875b81058fe230c509148c1861343a5f
Ronald Pereira <ronald.drp11@gmail.com>
2017-06-23 09:55:11 -0300

    Updated st-cut function

--- src/code/ford/ford.py
+++ src/code/ford/ford.py
@@ -36,7 +36,6 @@ class Ford:
         self.capacity = capacity # Stores the original capacity
         self.incidenceMatrix = incidenceMatrix # Stores the incidence matrix
         self.graph = self.incidenceMatrixToGraph() # Stores the graph into the object
-        self.flowGraph = self.incidenceMatrixToGraph() # Stores the final residual capacity graph
         self.dimx = len(self.graph) # Number of rows in graph
         self.dimy = len(self.graph[0]) # Number of columns in graph
         self.source = 0 # The source will be always the first vertice
@@ -112,7 +111,6 @@ class Ford:
                 s = self.parent[t] # s gets the previous vertice of the actual path
                 self.graph[s][t] -= pathFlow # Reduce the pathFlow of the edge
                 self.graph[t][s] += pathFlow # Add the pathFlow to the inverse edge
-                self.flowGraph[s][t] -= pathFlow
                 path = self.addActualFlow(s, t, pathFlow, path) # Adds the actual flow to the self.actualFlow vector and gets the actual path
                 t = self.parent[t] # t gets the previous vertice of the actual path (same as actual s)
 
@@ -136,20 +134,31 @@ class Ford:
         output.write("\n\n\n")
 
     def addSTCut(self, actual, vertice):
-        for column in range(len(self.incidenceMatrix[0])):
+        for column in range(len(self.incidenceMatrix[0])): # Search the edge in the incidenceMatrix
             if abs(self.incidenceMatrix[actual][column]) == 1 and abs(self.incidenceMatrix[vertice][column] == 1): # If it is -1 or 1
-                self.stCut[column] = 1
+                self.stCut[column] = 1 # Set the stCut vector to be 1 in that edge
 
     def printsSTCut(self, output):
         originalGraph = self.incidenceMatrixToGraph() # Gets original graph
         actual = self.source # Starts searching in the source vertice
-        for vertice in range(len(originalGraph[actual])): # Do a DFS for each valid edge in the originalGraph
-            if originalGraph[actual][vertice] > 0: # If the original capacity is > 0
-                if self.flowGraph[actual][vertice] == 0: # And the final residual capacity is 0
+        minCutStack = [] # Stack for the DFS minCut search
+        minCutStack.append(self.source) # Starts the search with the source vertice
+        vertice = 0 # Vertice counter
+        while minCutStack: # While the DFS minCutStack is not empty
+            if vertice == len(originalGraph[actual]) - 1: # If the vertice has reached the end of the graph checking of the actual vertice
+                actual = minCutStack.pop(-1) # The actual vertice is the
+                self.graph[actual][vertice] = -1 # Marks the edge, so it won't be visited again
+                vertice = -1 # Resets the vertice counter
+            elif originalGraph[actual][vertice] > 0: # If the original capacity is > 0
+                if self.graph[actual][vertice] == 0: # And the final residual capacity is 0
                     self.addSTCut(actual, vertice) # Adds the edge to the self.stCut vector
-                    actual = self.source # Reset the DFS to the source vertice
-                else: # If the final residual capacity is > 0
-                    actual = vertice # Do the DFS search on the next vertice
+                    self.graph[actual][vertice] = -1 # Marks the edge, so it won't be visited again
+                elif self.graph[actual][vertice] > 0: # If the final residual capacity is > 0
+                    self.graph[actual][vertice] = -1 # Marks the edge, so it won't be visited again
+                    minCutStack.append(vertice) # Appends the vertice to the stack
+                    actual = vertice # The actual vertice gets the vertice it goes
+                    vertice = -1 # Resets the vertice counter
+            vertice+=1 # Go to the next possible vertice
 
         for item in self.stCut:
             output.write("%d "%item)
